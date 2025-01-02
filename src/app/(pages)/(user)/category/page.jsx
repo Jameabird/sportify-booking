56a55fc2d5f699797f6852f45c51fd460bbdd104
Merlinz <105861613+Aisaraphorn18@users.noreply.github.com>
2025-01-02 12:41:20 +0700

    update

--- src/app/(pages)/(user)/category/page.jsx
+++ src/app/(pages)/(user)/category/page.jsx
@@ -5,52 +5,60 @@ import "./ChooseField.css";
 import TopBar from "@components/Topbar";
 
 const ChooseField = () => {
-    const [isClient, setIsClient] = useState(false); 
-    const [selectedField, setSelectedField] = useState(null);
-    const router = useRouter(); // ใช้ useRouter
-    const fields = new Set([
-      "Badminton",
-      "Football",
-      "Basketball",
-      "Golf",
-      "Swimming pool",
-      "Volleyball",
-      "Table Tennis",
-      "Hiking",
-      "Ice Skate",
-    ]);
+  const [isClient, setIsClient] = useState(false);
+  const [selectedField, setSelectedField] = useState(null);
+  const router = useRouter(); // ใช้ useRouter
+  const fields = new Set([
+    "Badminton",
+    "Football",
+    "Basketball",
+    "Golf",
+    "Swimming pool",
+    "Volleyball",
+    "Table Tennis",
+    "Hiking",
+    "Ice Skate"
+  ]);
 
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    const handleFieldSelection = (field) => {
-        setSelectedField(field);
-        if (isClient) {
-          router.push("/search");
-        }
-    };
-    
-    if (!isClient) {
-        return null;
+  const handleFieldSelection = field => {
+    setSelectedField(field);
+    if (isClient) {
+      router.push("/search");
     }
-    
+  };
+
+  if (!isClient) {
+    return null;
+  }
 
   const backgroundStyle = {
-    backgroundImage: "url('/gym_bg2.jpg')", 
+    backgroundImage: "url('/gym_bg2.jpg')",
     backgroundColor: "rgba(70, 80, 100, 0.7)",
-    backgroundBlendMode: "multiply", 
+    backgroundBlendMode: "multiply",
     opacity: 0.9,
-    zIndex: -1,
+    zIndex: -1
   };
 
   return (
-    <div>
-    <TopBar/>
-    <div className="choose-field-container">
+    <div
+     className="absolute top-0 left-0 h-full w-full bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/gym_bg2.jpg')",
+        backgroundColor: "rgba(70, 80, 100, 0.7)",
+        backgroundBlendMode: "multiply",
+        opacity: 0.9,
+        zIndex: -1
+      }}
+    >
+      <TopBar />
+      <div className="choose-field-container">
         <h1 className="title">CHOOSE FIELD</h1>
         <div className="fields">
-          {Array.from(fields).map((field, index) => (
+          {Array.from(fields).map((field, index) =>
             <button
               key={index}
               className="field-button"
@@ -58,7 +66,7 @@ const ChooseField = () => {
             >
               {field}
             </button>
-          ))}
+          )}
         </div>
       </div>
     </div>
--- src/app/(pages)/components/Topbar.jsx
+++ src/app/(pages)/components/Topbar.jsx
@@ -105,7 +105,7 @@ const TopBar = (props) => {
             Sign In
           </Button>
         </Box>
-</Link>
+{/* </Link> */}
 
       </Box>
     </Box>
