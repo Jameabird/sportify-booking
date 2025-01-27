f71ab7229a287c9b42bbb2cd0f4d2a1c28eb65aa
Siriwimon <siriwimon.sa@ku.th>
2025-01-27 22:42:11 +0700

    Payment_User_pages

---
+++ src/app/(pages)/(user)/payment/success.jsx/success.jsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import TopBar from "../../components/Topbar";
+import TopBar_User from "@components/Topbar_User";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  return (
+    <div className="app" style={{ display: "flex", height: "100vh" }}>
+      <main className="content" style={{ flex: 1, overflowY: "auto" }}>
+        {/* พื้นหลังเลเยอร์ */}
+        <div
+          className="absolute top-0 left-0 h-full w-full bg-cover bg-center"
+          style={{
+            backgroundImage: "url('/gym_bg2.jpg')",
+            backgroundColor: "rgba(70, 80, 100, 0.7)",
+            backgroundBlendMode: "multiply", 
+            opacity: 0.9,
+            zIndex: -1
+          }}
+        />
+        <div className="relative h-full w-full">
+        {/* เมื่อมีการใช้ครบ 12 ช่องแล้วจะสร้าง row ใหม่(บรรทัดใหม่) */}
+          <div className="grid grid-rows-auto gap-2">  
+            {/* สร้าง grid ให้มีขนาด 12 ช่องเท่าๆกัน ต่อ 1 บรรทัด  สามารถปรับลด ช่องได้ */}
+            <div className="grid grid-cols-12">
+              {/* จองพื้นที่ไว้ 12 คอลัมภ์ */}
+              <div className="col-span-12">
+                <TopBar_User textColor={"white"} />
+              </div>
+             
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
