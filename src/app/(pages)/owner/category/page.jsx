af95f9ea87596163db2e39c3344b8e92f3cadf12
Eutamber <kittiyaporn.du@ku.th>
2025-01-31 21:39:59 +0700

    เพิ่มโฟเดอร์ owner & topbar

---
+++ src/app/(pages)/owner/category/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import TopBar_Owner from "@components/Topbar_Owner";
+
+
+const CategoryPageOwner = () => { 
+  return (
+    <>
+      <TopBar_Owner textColor={"black"} />
+     
+    </>
+  );
+};
+
+export default CategoryPageOwner;
