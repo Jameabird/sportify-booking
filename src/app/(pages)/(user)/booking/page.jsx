ed0dd03e02b615c5fed39d01f7c76a61accff299
Meowzaza007 <chayanin.ta@ku.th>
2025-02-04 17:32:43 +0700

    deleted:    src/app/(pages)/(user)/booking/page copy 4.jsx 	modified:   src/app/(pages)/(user)/booking/page.jsx

--- src/app/(pages)/(user)/booking/page.jsx
+++ src/app/(pages)/(user)/booking/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import TopBar from "@components/Topbar";
 import {
   Box,
@@ -508,15 +509,15 @@ const Booking = () => {
               &times;
             </button>
             <div className="header"> รายละเอียดการจอง </div>
-            <div className="content">
-              QR CODE.
-            </div>
-            <div className="actions">
-              <button className="button" onClick={() => {
-                console.log('Button clicked');
-                close();
-              }}>Click here</button>
-            </div>
+            <ul>
+            <li className="section"><h2>สถานที่</h2></li>
+            <li className="section"><h2>วันที่</h2></li>
+            <li className="section"><h2>เวลา</h2></li>
+            <li className="section"><h2>ราคารวม</h2></li>
+            <li className="section">QR CODE</li>
+            </ul>
+            <div><Button sx={{ backgroundColor: "green", color: "black", width: "25%" }}>Booking</Button>
+            <Button sx={{ backgroundColor: "red", color: "black", width: "25%" }}>ยกเลิก</Button></div>
           </div>
         )}
       </Popup>
