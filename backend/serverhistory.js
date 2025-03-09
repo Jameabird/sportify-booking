cba80e71464d71c8a2f725f562a26660a118e558
Jameabird <JameabirdTAT@gmail.com>
2025-03-09 18:08:00 +0700

    fix port

--- backend/serverhistory.js
+++ backend/serverhistory.js
@@ -114,7 +114,7 @@ app.get("/api/history", authenticate, async (req, res) => {
 });
 
 
-const PORT = process.env.PORT6 || 4000;
+const PORT = process.env.PORT6 || 4003;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
